aaf7b37fb2b514d33f3527b094bfc7a61aae78d2
Renata <rselleshiy@gmail.com>
2023-02-14 17:17:38 -0800

    moved index.js to outermost folder

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { data } = require('../data/data.json');
+const { data } = require('./data/data.json');
 const { projects } = data;
 
 //Setting the routes
